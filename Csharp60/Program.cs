8229fb20fe9797c261e4ee9e1b78680909b3c3be
Viktor Török <torokv@vnet.hu>
2022-11-07 15:22:26 +0100

    Adding StringInterpolation() method

--- Csharp60/Program.cs
+++ Csharp60/Program.cs
@@ -10,8 +10,16 @@ namespace Csharp60
 {
     class Program
     {
+        private static void Init()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
         private static void WriteLineUsingStatic()
         {
+            Console.WriteLine("1. Using static type directive");
+
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("Console.WriteLine");
@@ -23,11 +31,26 @@ namespace Csharp60
             WriteLine();
         }
 
+        private static void StringInterpolation()
+        {
+            Console.WriteLine("2. String interpolation");
+
+            int a = 8;
+            int b = 7;
+
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            Console.WriteLine(string.Format("{0} + {1} = {2}", a, b, a + b));
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a:#0.0} + {b:#0.0} = {a + b:#0.0}");
+        }
+
         static void Main(string[] args)
         {
+            Init();
+            
             // C# 6.0 examples
-
             WriteLineUsingStatic();
+            StringInterpolation();
         }
     }
 }
