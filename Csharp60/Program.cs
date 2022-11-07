8eb944c832519b9ee62e456a88a53547e9517e18
Viktor Török <torokv@vnet.hu>
2022-11-07 15:11:24 +0100

    Add project files.

---
+++ Csharp60/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
+
+namespace Csharp60
+{
+    class Program
+    {
+        private static void WriteLineUsingStatic()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Console.WriteLine");
+            Console.WriteLine();
+
+            BackgroundColor = Black;
+            ForegroundColor = Gray;
+            WriteLine("WriteLine");
+            WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLineUsingStatic();
+        }
+    }
+}
